8342d4941233f327523415423712304694edb8db
Jeffrey Ng <jeffreymng@netzero.net>
2016-10-19 19:11:01 -0400

    initial commit, header css partially coded

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="/css/styles.css">
+</head>
+<body>
+  <div class='container center'>
+    <div class='col-xs-6'>
+      <h3 class='no-margin-bottom'><span class=red>PRO</span><span class='blue'>FESSIONAL</span></h3>
+      <h1 class='blue no-margin-top'>PAINTERS</h1>
+    </div>
+    <div class='col-xs-6'>
+      <h3 style='text-align: right;'><span class='blue'>CALL <span class='red'>555-123-4567</span></h3>
+    </div>
+  </div>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php' ?>
+
+<div class='fluid-container'>
+
+</div>
+
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php' ?>
