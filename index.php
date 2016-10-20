eb500cf2033f97b2006ebb7a8f82e238f1cafbef
Jeffrey Ng <jeffreymng@netzero.net>
2016-10-19 21:26:17 -0400

    added image to page, fixed styling so image stays in background

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
+
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php' ?>
 
-<div class='container-fluid' id='top-red bottom-blue'>
+<div class='container-fluid' id='top-red'>
+  <img src='/img/house.jpg' alt='house'>
+
 
 </div>
+
+
 <div class='container-fluid'>
   
 </div>
